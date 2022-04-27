15ffe08d351c228027f82a3c24e0f2a3c5c74cc4
Elias Lima <38722114+EliasRLima@users.noreply.github.com>
2022-04-27 20:48:09 -0300

    correcao-08

--- src/atv02/questao08/CalculaDesconto.java
+++ src/atv02/questao08/CalculaDesconto.java
@@ -2,14 +2,11 @@ package atv02.questao08;
 
 import atv02.questao08.estrategias.ConcreteEventos;
 import atv02.questao08.estrategias.DescontoContexto;
+import atv02.questao08.estrategias.DescontoEstrategia;
 
 public class CalculaDesconto {
 
     private DescontoContexto descontoContexto;
-
-    public CalculaDesconto() {
-
-    }
 
     //o metodo calcular funciona para todos os produtos
     public double calcular(double valorOriginal){
@@ -20,6 +17,12 @@ public class CalculaDesconto {
     public double calcular(double valorOriginal,double descontoEspecial){
         descontoContexto.setDescontoEstrategia(new ConcreteEventos(descontoEspecial));
         //apenas salva o desconto, colocando contexto como evento e executa normalmente
+        return calcular(valorOriginal);
+    }
+
+    //passando o produto como se tivesse sem evento ativo (preço classico de desconto)
+    public double calcular(double valorOriginal, DescontoEstrategia descontoEstrategia){
+        descontoContexto.setDescontoEstrategia(descontoEstrategia);
         return calcular(valorOriginal);
     }
 }
